0b77731eb0ddfd425f6810a9a5ce25fc008ce0fd
mrosas90s <mrosas@losalercespuertomontt.cl>
2023-07-10 21:04:28 -0400

    primer commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :likes
+  resources :posts do
+    post 'like', on: :member
+    get 'update_likes', on: :member
+  end
+  
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root 'posts#index'
+
+end
